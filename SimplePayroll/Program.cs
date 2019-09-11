8eb7a35c25a2388999e41fb0ece08ac13b8eed8b
Luke Fiji <luke.fiji@gmail.com>
2019-09-10 22:10:23 -0700

    Implement FileReader class

--- SimplePayroll/Program.cs
+++ SimplePayroll/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace SimplePayroll
 {
@@ -79,11 +81,68 @@ namespace SimplePayroll
 
     class Admin : Staff
     {
+        private const float overtimeRate = 15.5f;
+        private const float adminHourlyRate = 30;
+
+        public float Overtime { get; private set; }
+
+        public Admin (string name) : base(name, adminHourlyRate) { }
+
+        public override void CalculatePay()
+        {
+            base.CalculatePay();
+
+            if (HoursWorked > 160)
+            {
+                Overtime = overtimeRate * (HoursWorked - 160);
+                TotalPay = BasicPay + Overtime;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + NameOfStaff + "\n Admin Hourly Rate: " + adminHourlyRate + "\nHours Worked: " + HoursWorked  + "\n Overtime Rate: " + overtimeRate + "\nTotal Pay: " + TotalPay + "\n Basic Pay: " + BasicPay;
+        }
 
     }
 
     class FileReader
     {
+        public List<Staff> ReadFile()
+        {
+            List<Staff> myStaff = new List<Staff>();
+            string[] result = new string[2];
+            string path = "staff.txt";
+            string[] separator = { ", " };
+
+            if (File.Exists(path))
+            {
+                // Using ensures that Dispose() method is always called
+                // - Closes/releases any unmanaged resources such as files & streams
+                using (StreamReader sr=new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        result = sr.ReadLine().Split(separator, System.StringSplitOptions.None);
+                        if (result[1] == "Admin")
+                        {
+
+                        } else if (result[1] == "Manager")
+                        {
+
+                        } else
+                        {
+
+                        }
+                    }
+                    // Close file so other programs can use it
+                    sr.Close();
+                }
+            } else
+            {
+                Console.WriteLine("Error: Staff file not found");
+            }
+        }
 
     }
 
