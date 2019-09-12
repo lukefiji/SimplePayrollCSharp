e531e80712db710de7275ea802016f6837dad2d8
Luke Fiji <luke.fiji@gmail.com>
2019-09-11 22:22:00 -0700

    Implement GenerateSummary and ToStting of PaySlip class

--- SimplePayroll/Program.cs
+++ SimplePayroll/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace SimplePayroll
 {
@@ -86,7 +87,7 @@ namespace SimplePayroll
 
         public float Overtime { get; private set; }
 
-        public Admin (string name) : base(name, adminHourlyRate) { }
+        public Admin(string name) : base(name, adminHourlyRate) { }
 
         public override void CalculatePay()
         {
@@ -101,7 +102,7 @@ namespace SimplePayroll
 
         public override string ToString()
         {
-            return "Name: " + NameOfStaff + "\n Admin Hourly Rate: " + adminHourlyRate + "\nHours Worked: " + HoursWorked  + "\n Overtime Rate: " + overtimeRate + "\nTotal Pay: " + TotalPay + "\n Basic Pay: " + BasicPay;
+            return "Name: " + NameOfStaff + "\n Admin Hourly Rate: " + adminHourlyRate + "\nHours Worked: " + HoursWorked + "\n Overtime Rate: " + overtimeRate + "\nTotal Pay: " + TotalPay + "\n Basic Pay: " + BasicPay;
         }
 
     }
@@ -119,35 +120,122 @@ namespace SimplePayroll
             {
                 // Using ensures that Dispose() method is always called
                 // - Closes/releases any unmanaged resources such as files & streams
-                using (StreamReader sr=new StreamReader(path))
+                using (StreamReader sr = new StreamReader(path))
                 {
                     while (!sr.EndOfStream)
                     {
                         result = sr.ReadLine().Split(separator, System.StringSplitOptions.None);
-                        if (result[1] == "Admin")
-                        {
+                        string name = result[0];
+                        string role = result[1];
 
-                        } else if (result[1] == "Manager")
+                        if (role == "Admin")
                         {
-
-                        } else
+                            myStaff.Add(new Admin(name));
+                        }
+                        else if (role == "Manager")
                         {
-
+                            myStaff.Add(new Manager(name));
                         }
                     }
                     // Close file so other programs can use it
                     sr.Close();
                 }
-            } else
+            }
+            else
             {
                 Console.WriteLine("Error: Staff file not found");
             }
+
+            return myStaff;
         }
 
     }
 
     class PaySlip
     {
+        private int month;
+        private int year;
 
+        // Enums declared inside a class are private by default
+        enum MonthsOfYear
+        {
+            JAN = 1,
+            FEB = 2,
+            MAR = 3,
+            APR = 4,
+            MAY = 5,
+            JUN = 6,
+            JUL = 7,
+            AUG = 8,
+            SEP = 9,
+            OCT = 10,
+            NOV = 11,
+            DEC = 12
+        }
+
+        public PaySlip(int payMonth, int payYear)
+        {
+            month = payMonth;
+            year = payYear;
+        }
+
+        public void GeneratePaySlip(List<Staff> myStaff)
+        {
+            string path;
+
+            foreach (Staff f in myStaff)
+            {
+                path = f.NameOfStaff + ".txt";
+
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    sw.WriteLine("PAYSLIP FOR {0} {1}", (MonthsOfYear)month, year);
+                    sw.WriteLine("==============================");
+                    sw.WriteLine("Name Of Staff: {0}", f.NameOfStaff);
+                    sw.WriteLine("Hours Worked: {0}", f.HoursWorked);
+                    sw.WriteLine("");
+                    sw.WriteLine("Basic Pay: {0:C}", f.BasicPay);
+
+                    if (f.GetType() == typeof(Manager))
+                        sw.WriteLine("Allowance: {0:C}", ((Manager)f).Allowance);
+                    else if (f.GetType() == typeof(Admin))
+                        sw.WriteLine("Overtime: {0:C}", ((Admin)f).Overtime);
+
+                    sw.WriteLine("");
+                    sw.WriteLine("==============================");
+                    sw.WriteLine("Total Pay: {0}", f.TotalPay);
+                    sw.WriteLine("==============================");
+                    sw.Close();
+                }
+            }
+        }
+
+        public void GenerateSummary(List<Staff> staff)
+        {
+            var result =
+                from employee in staff
+                where employee.HoursWorked < 0
+                orderby employee.NameOfStaff ascending
+                select new { employee.NameOfStaff, employee.HoursWorked };
+
+            string path = "summary.txt";
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("Staff with less than 10 working hours");
+                sw.WriteLine("");
+
+                foreach (var employee in result)
+                {
+                    Console.WriteLine("Name of Staff: {0}, Hours Worked: {1}", employee.NameOfStaff, employee.HoursWorked);
+                }
+
+                sw.Close();
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Month = " + month + ", Year = " + year;
+        }
     }
 }
